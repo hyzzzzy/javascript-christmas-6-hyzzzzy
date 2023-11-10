677e89e77bc34c6c1d097828f7548d03ae072ce1
hyzzzzy <shinhj4068@naver.com>
2023-11-11 00:09:36 +0900

    refactor(setting): 상수 세팅 파일 분리 및 날짜 분리

---
+++ src/constants/setting.js
@@ -0,0 +1,13 @@
+export const SETTING = Object.freeze({
+  min_menu: 1,
+  order_partial_length: 2,
+  max_menu: 20,
+  name: '우테코 식당',
+});
+
+export const DATE = Object.freeze({
+  min_day: 1,
+  month: 12,
+  max_day: 31,
+  year: 2023,
+});
