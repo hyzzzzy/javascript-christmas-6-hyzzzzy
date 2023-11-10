fee4d06619eef85076d3a5ac833c2f44bbd47f29
hyzzzzy <shinhj4068@naver.com>
2023-11-10 23:12:27 +0900

    feat(InputView): 날짜 입력, 메뉴 입력 메서드 구현

---
+++ src/view/InputView.js
@@ -0,0 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../constants/message';
+import Validate from '../utils/Validate';
+
+const InputView = {
+  async readDay() {
+    const input = await Console.readLineAsync(MESSAGE.input_day);
+    Validate.isDay(input);
+    Validate.isNaturalNumber(input);
+    return Number(input);
+  },
+
+  async readMenu() {
+    const input = await Console.readLineAsync(MESSAGE.input_menu);
+    Validate.hasMenu(input);
+    Validate.hasMinQuantity(input);
+    Validate.isMenuFormat(input);
+    Validate.isUniqueMenu(input);
+    Validate.isOnlyBeverage(input);
+    Validate.isTooManyQuantity(input);
+  },
+}
+
+export default InputView;
