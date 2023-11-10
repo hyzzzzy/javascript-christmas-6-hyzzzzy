c51c454cfaaca8b1ef63bf2b6cf95e60d49056fc
hyzzzzy <shinhj4068@naver.com>
2023-11-10 21:42:46 +0900

    style: util 파일 삭제

--- src/utils/util.js
+++
@@ -1,9 +0,0 @@
-export const deepFreeze = (obj) => {
-  Object.keys(obj).forEach((key) => {
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
-      deepFreeze(obj[key]);
-    }
-  });
-
-  Object.freeze(obj);
-}
