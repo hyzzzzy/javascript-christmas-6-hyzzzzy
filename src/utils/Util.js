5a39c4b363035edcbc1766a671a55341078d7fde
hyzzzzy <shinhj4068@naver.com>
2023-11-11 03:33:30 +0900

    fix(Util): 누락된 this 키워드 추가

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -2,7 +2,7 @@ const Util = {
   deepFreeze(obj) {
     Object.keys(obj).forEach((key) => {
       if (typeof obj[key] === 'object' && obj[key] !== null) {
-        deepFreeze(obj[key]);
+        this.deepFreeze(obj[key]);
       }
     });
   
