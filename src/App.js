e9b154a456fb1011448890ef38a3d97692a48e46
hyzzzzy <shinhj4068@naver.com>
2023-11-11 15:19:27 +0900

    feat(App): 증정 이벤트 로직 구현

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Console } from '@woowacourse/mission-utils';
 import InputView from './view/InputView.js';
 import OutputView from './view/OutputView.js';
 import Order from './domain/Order.js';
+import Promotion from './domain/Promotion.js';
 
 class App {
   #order;
+  #promotion;
 
   async run() {
     OutputView.printIntro();
@@ -12,12 +14,20 @@ class App {
     OutputView.printPreview(this.#order.date);
     OutputView.printMenu(this.#order.menu);
     OutputView.printPriceBeforeDiscount(this.#order);
+    this.printGiftList();
   }
   
   async makeReservation() {
     const day = await this.readUserInput(InputView.readDay);
     const menu = await this.readUserInput(InputView.readMenu);
     this.#order = new Order(day, menu);
+  }
+
+  printGiftList() {
+    this.#promotion = new Promotion();
+    const price = this.#order.calculateTotalPrice();
+    const isOverPrice = this.#promotion.calculateGiftEvent(price);
+    OutputView.printGiftMenu(isOverPrice);
   }
 
   async readUserInput(inputFunction, functionParameter) {
