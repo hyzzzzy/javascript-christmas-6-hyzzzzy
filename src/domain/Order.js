7e4d37ee7239e961fc0767b8090e3637a341f878
hyzzzzy <shinhj4068@naver.com>
2023-11-11 13:40:16 +0900

    feat(Order): 주문 계산 메서드 구현 및 date 구조 변경

--- src/domain/Order.js
+++ src/domain/Order.js
@@ -1,12 +1,41 @@
-import { DATE } from '../constants/setting.js';
+import Validate from '../utils/Validate';
 
 class Order {
   #date;
   #menu;
-
+  
   constructor(day, menu) {
-    this.#date = new Date(DATE.year, DATE.month - 1, day);
+    this.#date = day;
     this.#menu = menu;
+  }
+
+  get date() {
+    return this.#date;
+  }
+
+  get menu() {
+    return this.#menu;
+  }
+
+  calculatePrice(category, quantity) {
+    let price = 0;
+    if (category) {
+      price += category.price * quantity;
+    }
+    return price;
+  }
+
+  calculateTotalPrice() {
+    let price = 0;
+    for (const order of this.#menu) {
+      const [appetizer, main, dessert, beverage] = Validate.hasMenuInCategory(order.menu);
+
+      price += this.calculatePrice(appetizer, order.quantity);
+      price += this.calculatePrice(main, order.quantity);
+      price += this.calculatePrice(dessert, order.quantity);
+      price += this.calculatePrice(beverage, order.quantity);
+    }
+    return price;
   }
 }
 
