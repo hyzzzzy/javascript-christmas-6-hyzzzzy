b77d9b17372ff6c6c4c439ab5dccc0239d49aa14
hyzzzzy <shinhj4068@naver.com>
2023-11-11 10:30:49 +0900

    feat(App): 날짜, 메뉴 입력 구현 및 Order 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputView from './view/InputView.js';
+import OutputView from './view/OutputView.js';
+import Order from './domain/Order.js';
+
 class App {
-  async run() {}
+  #order;
+
+  async run() {
+    OutputView.printIntro();
+    await this.makeReservation();
+  }
+  
+  async makeReservation() {
+    const day = await this.readUserInput(InputView.readDay);
+    const menu = await this.readUserInput(InputView.readMenu);
+    this.#order = new Order(day, menu);
+  }
+
+  async readUserInput(inputFunction, functionParameter) {
+    try {
+      return await inputFunction(functionParameter);
+    } catch (e) {
+      Console.print(e.message);
+      return this.readUserInput(inputFunction, functionParameter);
+    }
+  }
 }
 
 export default App;
