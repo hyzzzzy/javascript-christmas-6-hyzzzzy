6a4d068e2cc9cafa05eb369bcd8cc109a511d0b4
hyzzzzy <shinhj4068@naver.com>
2023-11-11 18:31:29 +0900

    feat(event): 혜택 계산을 위한 PRICE 상수 설정 및 DATE 객체 이동

--- src/constants/event.js
+++ src/constants/event.js
@@ -1,10 +1,24 @@
 import Util from '../utils/Util';
 
+export const DATE = Object.freeze({
+  min_day: 1,
+  month: 12,
+  max_day: 31,
+  year: 2023,
+});
+
 export const SPECIAL_DAY = Object.freeze([
   3, 10, 17, 24, 25, 31,
 ]);
 
-export const PRICE_FOR_GIFT = Object.freeze(120000);
+export const PRICE = Object.freeze({
+  for_gift: 120000,
+  for_benefit: 10000,
+  for_d_day_init: 1000,
+  for_d_day_increase: 100,
+  for_day_of_week: DATE.year,
+  for_special: 1000,
+});
 
 export const BADGE = {
   star: { name: '별', price: 5000 },
