ff2d5cd3f006c0a30105a0f4b0b3d91ff8a4f62e
hyzzzzy <shinhj4068@naver.com>
2023-11-12 15:15:14 +0900

    feat(Promotion): 혜택 내역 계산 메서드 구현 크리스마스 디데이 할인 계산 메서드 구현 평일 할인 계산 메서드 구현 주말 할인 계산 메서드 구현 특별 할인 계산 메서드 구현 증정 이벤트 계산 메서드 구현

--- src/domain/Promotion.js
+++ src/domain/Promotion.js
@@ -1,11 +1,80 @@
-import { PRICE_FOR_GIFT } from '../constants/event';
+import { DATE, PRICE, SPECIAL_DAY } from '../constants/event';
+import { BEVERAGE } from '../constants/menu';
+import Validate from '../utils/Validate';
 
 class Promotion {
   calculateGiftEvent(price) {
-    if (price > PRICE_FOR_GIFT) {
+    if (price > PRICE.for_gift) {
       return true;
     }
     return false;
+  }
+
+  calculateDDay(price, day) {
+    let discount = 0;
+    if (price < PRICE.for_benefit) {
+      return 0;
+    }
+
+    discount = PRICE.for_d_day_init + (day - 1) * PRICE.for_d_day_increase;
+
+    return discount * (-1);
+  }
+
+  calculateWeekday(date, orders) {
+    let discount = 0;
+    const reservationDate = new Date(DATE.year, DATE.month - 1, date);
+    const day = reservationDate.getDay();
+
+    if (day === 5 || day === 6) {
+      return 0;
+    }
+
+    for (const order of orders) {
+      const { dessert } = Validate.hasMenuInCategory(order.menu);
+      if (dessert) {
+        discount += order.quantity * DATE.year;
+      }
+    }
+
+    return discount * (-1);
+  }
+
+  calculateWeekend(date, orders) {
+    let discount = 0;
+    const reservationDate = new Date(DATE.year, DATE.month - 1, date);
+    const day = reservationDate.getDay();
+
+    if (day !== 5 && day !== 6) {
+      return 0;
+    }
+
+    for (const order of orders) {
+      const { main } = Validate.hasMenuInCategory(order.menu);
+      if (main) {
+        discount += order.quantity * DATE.year;
+      }
+    }
+
+    return discount * (-1);
+  }
+
+  calculateSpecialDay(date) {
+    const isSpecial = SPECIAL_DAY.includes(date);
+
+    if (isSpecial) {
+      return PRICE.for_special * (-1);
+    }
+
+    return 0;
+  }
+
+  calculateGiftPrice(price) {
+    if (price > PRICE.for_gift) {
+      return BEVERAGE.champagne.price * (-1);
+    }
+
+    return 0;
   }
 }
 
